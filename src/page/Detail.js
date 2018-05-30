7000c998b3ac60f3d940bc2b55e03d56ab526e06
spicyboiledfish <327434855@qq.com>
2018-05-30 22:16:00 +0800

    ...

--- src/page/Detail.js
+++ src/page/Detail.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, StatusBar } from 'react-native'
+import {Color} from 'LocalReference';
 
 export default class Detail extends Component {
     static navigationOptions = {
         title: '第二页',
         headerBackTitle: null,
-        headerTintColor:'#fff',
       };
   render() {
     return (
       <View style={{justifyContent:'center',flex:1,alignItems:'center'}}>
+        <StatusBar
+            backgroundColor={Color.HSHeaderBgColor}
+            barStyle="default"
+        />
         <Text> Detail </Text>
       </View>
     )
